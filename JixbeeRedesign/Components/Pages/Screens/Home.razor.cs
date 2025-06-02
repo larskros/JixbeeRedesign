02b114afda9badc1647db576aa0ea777ea603454
larskros <l.kros@student.fontys.nl>
2025-06-02 16:02:13 +0200

    Payout popup component

--- JixbeeRedesign/Components/Pages/Screens/Home.razor.cs
+++ JixbeeRedesign/Components/Pages/Screens/Home.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Globalization;
 
 namespace JixbeeRedesign.Components.Pages.Screens
 {
@@ -7,6 +8,7 @@ namespace JixbeeRedesign.Components.Pages.Screens
         [Parameter] public string? Class { get; set; }
         [Parameter] public EventCallback<int> ActiveIndexChanged { get; set; }
         [Parameter] public int InitialIndex { get; set; }
+        [Parameter] public EventCallback<int> WithdrawAmountChanged { get; set; }
 
         private List<string> SegmentItems = new()
         {
@@ -15,6 +17,16 @@ namespace JixbeeRedesign.Components.Pages.Screens
 
         private int activeIndex = 0;
 
+        private bool showPopup { get; set; }
+        private int popupHeight { get; set; } = 0;
+        private string popupBgColor { get; set; } = "rgba(31, 31, 31, 0)";
+        private string visible { get; set; } = "hidden";
+        private string popupButtonText { get; set; } = "Opnemen";
+        private int MaximumSalary { get; set; } = 1250;
+        private int WithdrawAmount { get; set; }
+
+
+
         private async Task OnActiveIndexChanged(int index)
         {
             activeIndex = index;
@@ -22,8 +34,35 @@ namespace JixbeeRedesign.Components.Pages.Screens
             StateHasChanged();
         }
 
-        private static void HandleWithdraw()
+        private void HandleWithdraw()
         {
+            showPopup = true;
+            popupHeight = 90;
+            popupBgColor = "rgba(31, 31, 31, 0.5)";
+			visible = "visible";
+			StateHasChanged();
+		}
+
+        private void SetMaximumAvailableSalary()
+        {
+
+        }
+
+        private async void ClosePopup()
+        {
+            showPopup = false;
+            popupHeight = 0;
+			popupBgColor = "rgba(31, 31, 31, 0)";
+			await Task.Delay(350);
+			visible = "hidden";
+            StateHasChanged();
+		}
+
+        private async Task OnWithdrawInputChanged(int index)
+        {
+            WithdrawAmount = index;
+            await WithdrawAmountChanged.InvokeAsync(index);
+            StateHasChanged();
 
         }
     }
