8545828b36a88b5682e9f8181c54748fff2e51d4
Lars Kros <93661874+larskros@users.noreply.github.com>
2025-06-06 13:00:11 +0200

    Payout history page

---
+++ JixbeeRedesign/Components/Pages/Screens/PayoutHistory.razor.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace JixbeeRedesign.Components.Pages.Screens
+{
+    public partial class PayoutHistory
+    {
+        [Parameter] public string? Class { get; set; }
+        [Parameter] public EventCallback<int> ActiveIndexChanged { get; set; }
+        [Parameter] public int InitialIndex { get; set; }
+
+        private List<Payout> allPayouts = new List<Payout>();
+
+        private bool showPopup { get; set; }
+        private int popupHeight { get; set; } = 0;
+        private string popupBgColor { get; set; } = "rgba(31, 31, 31, 0)";
+        private string visible { get; set; } = "hidden";
+        private string popupButtonText { get; set; } = "Inplannen";
+        private string? recurringPaymentAmount { get; set; }
+
+        public class Payout
+        {
+            public string? Title { get; set; }
+            public string? Type { get; set; }
+            public int Amount { get; set; }
+            public DateTime? Date { get; set; }
+        }
+
+        private int activeIndex = 0;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadRecurringPayments();
+        }
+
+        private async Task LoadRecurringPayments()
+        {
+            allPayouts = new List<Payout>
+            {
+                new Payout
+                {
+                    Title = "Koffie budget",
+                    Type = "Geplande opname",
+                    Amount = 40,
+                    Date = new DateTime(2025, 04, 28)
+                },
+                new Payout
+                {
+                    Title = "Koffie budget",
+                    Type = "Geplande opname",
+                    Amount = 650,
+                    Date = new DateTime(2025, 04, 23)
+                },
+                new Payout
+                {
+                    Title = "Telefoon abonnement",
+                    Type = "Geplande opname",
+                    Amount = 25,
+                    Date = new DateTime(2025, 04, 09)
+                },
+                new Payout
+                {
+                    Title = "Koffie budget",
+                    Type = "Geplande opname",
+                    Amount = 40,
+                    Date = new DateTime(2025, 03, 28)
+                },
+                new Payout
+                {
+                    Title = "Koffie budget",
+                    Type = "Geplande opname",
+                    Amount = 650,
+                    Date = new DateTime(2025, 03, 23)
+                },
+                new Payout
+                {
+                    Title = "Koffie budget",
+                    Type = "Geplande opname",
+                    Amount = 40,
+                    Date = new DateTime(2025, 03, 09)
+                }
+            };
+        }
+    }
+}
--- JixbeeRedesign/Components/Pages/Screens/RecurringPayments.razor.cs
+++ JixbeeRedesign/Components/Pages/Screens/RecurringPayments.razor.cs
@@ -18,6 +18,10 @@ namespace JixbeeRedesign.Components.Pages.Screens
         private string? recurringPaymentAmount { get; set; }
         private int[] days = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28 };
 
+        private string? recurringPaymentTitle { get; set; }
+        private string? RecurringPaymentTitleClass { get; set; }
+        private bool hasEndDate { get; set; }
+
         public class RecurringPayment
         {
             public string? Title { get; set; }
@@ -83,6 +87,23 @@ namespace JixbeeRedesign.Components.Pages.Screens
             popupBgColor = "rgba(31, 31, 31, 0)";
             await Task.Delay(350);
             visible = "hidden";
+            StateHasChanged();
+        }
+
+        private void HandleTitleInput(string value)
+        {
+            Console.WriteLine(recurringPaymentTitle);
+            recurringPaymentTitle = value;
+            Console.WriteLine(recurringPaymentTitle);
+            if(string.IsNullOrEmpty(value))
+            {
+                RecurringPaymentTitleClass = "none";
+                Console.WriteLine(recurringPaymentTitle);
+            }
+            else
+            {
+                RecurringPaymentTitleClass = "filled-in-title";
+            }
             StateHasChanged();
         }
     }
