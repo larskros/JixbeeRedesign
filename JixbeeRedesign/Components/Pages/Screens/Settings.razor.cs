04b1c5064c8630625f0712a7c3389342df75d869
Lars Kros <93661874+larskros@users.noreply.github.com>
2025-06-13 17:45:45 +0200

    Settings page

---
+++ JixbeeRedesign/Components/Pages/Screens/Settings.razor.cs
@@ -0,0 +1,24 @@
+﻿using JixbeeRedesign.Models;
+using JixbeeRedesign.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace JixbeeRedesign.Components.Pages.Screens
+{
+    public partial class Settings
+    {
+        [Parameter] public string? Class { get; set; }
+        [Parameter] public EventCallback<int> ActiveIndexChanged { get; set; }
+        [Parameter] public int InitialIndex { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        private string preferredLanguage { get; set; }
+        private string[] languages = { "nl", "en"};
+
+        private int activeIndex = 0;
+
+        protected override async Task OnInitializedAsync()
+        {
+            preferredLanguage = "nl";
+        }
+    }
+}
